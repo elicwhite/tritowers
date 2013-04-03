3aa673ce3c674f0ec348544000986d4b56f01935
TheSavior <github@eli-white.com>
2013-04-03 15:43:08 -0700

    Splitting AStar out

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <link type="text/css" rel="stylesheet" href="styles.css"></style>
 <script src="closure-library/closure/goog/base.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+<script src="AStar.js"></script>
 <script src="search.js"></script>
 </head>
 <body>
--- search.js
+++ search.js
@@ -9,11 +9,14 @@ $(function() {
 
     var currentCell = matrix[0][0];
 
+    var pathFinding;
+
     function buildBoard(rows, cols) {
         var matrix = [];
 
         var box = document.getElementById("box");
-        //console.log(box);
+
+        pathFinding = new AStar(matrix);
 
         for (var row = 0; row < rows; row++) {
             matrix[row] = [];
@@ -43,111 +46,13 @@ $(function() {
             for (var j = 0; j < lines[i].childNodes.length; j++) {
                 lines[i].childNodes[j].className = "cell";
             }
-
         }
 
         // Navigate to this cell
-        var path = astar(this);
+        var path = pathFinding.findPath(currentCell, this);
 
         path.forEach(function(item) {
             $(document.getElementById(item)).addClass("path");
         });
     }
-
-    // navigate to cell from currentCell
-
-    function astar(goal) {
-        var start = currentCell;
-
-        var closedSet = [];
-        var openSet = new goog.structs.PriorityQueue();
-
-        var cameFrom = [];
-
-        var g_score = [];
-        var f_score = [];
-        g_score[start] = 0;
-
-        f_score[start] = g_score[start] + manhattan_distance(start, goal);
-
-        // enqueue the start cell
-        openSet.enqueue(f_score[start], currentCell);
-
-        while (!openSet.isEmpty()) {
-            var current = openSet.dequeue();
-            // style the ones we've processed
-            $(current).addClass("checked");
-            if (current == goal) {
-                return reconstructPath(cameFrom, goal.id);
-            }
-
-            closedSet.push(current);
-
-            var neighbors = getNeighbors(current);
-
-            for (var i = 0; i < neighbors.length; i++) {
-                var neighbor = neighbors[i];
-                var temp_g_score = g_score[current] + manhattan_distance(current, neighbor);
-                if (closedSet.indexOf(neighbor) >= 0) {
-                    if (temp_g_score >= g_score[neighbor]) {
-                        continue;
-                    }
-                }
-
-                if (!openSet.containsValue(neighbor) || temp_g_score < g_score[neighbor]) {
-                    cameFrom[neighbor.id] = current.id;
-                    g_score[neighbor] = temp_g_score;
-                    f_score[neighbor] = g_score[neighbor] + manhattan_distance(neighbor, goal);
-                    if (!openSet.containsValue(neighbor)) {
-                        openSet.enqueue(f_score[neighbor], neighbor);
-                    }
-                }
-            }
-        }
-
-        return false;
-    }
-
-    function manhattan_distance(start, goal) {
-        var dist = Math.abs(goal.row - start.row) + Math.abs(goal.col - start.col);
-        return dist;
-    }
-
-    function reconstructPath(cameFrom, currentId) {
-        if (currentId in cameFrom) {
-            var p = reconstructPath(cameFrom, cameFrom[currentId]);
-            p.push(currentId);
-            return p;
-        }
-        return [currentId];
-    }
-
-    function getNeighbors(cell) {
-        var neighbors = [];
-
-        // check neighbors
-
-        // top
-        if (cell.row - 1 >= 0) {
-            neighbors.push(matrix[cell.row - 1][cell.col]);
-        }
-
-        // left
-        if (cell.col - 1 >= 0) {
-            neighbors.push(matrix[cell.row][cell.col - 1]);
-        }
-
-        // right
-        if (cell.col + 1 < matrix[0].length) {
-            neighbors.push(matrix[cell.row][cell.col + 1]);
-        }
-
-        // bottom
-        if (cell.row + 1 < matrix.length) {
-            neighbors.push(matrix[cell.row + 1][cell.col]);
-        }
-
-        return neighbors;
-    }
-
 });
